8d044ea9e5669924f7c842fa0d95d0050bae212f
David Rodriguez <david.rodriguez@dnnsoftware.com>
2016-05-30 16:28:05 +0100

    Added @jbrinkman SPA module, super fancy stuff

---
+++ Dnn.Bot/scripts/ItemView.js
@@ -0,0 +1,27 @@
+﻿var dnnBot = dnnBot || {
+    showTab: function () {
+        $('.bot-tab').removeClass('bot-tab-hide');
+    },
+    hideTab: function () {
+        $('.bot-tab').addClass('bot-tab-hide');
+    },
+    showChat: function () {
+        $('.bot-chat').addClass('bot-chat-show');
+    },
+    hideChat: function () {
+        $('.bot-chat').removeClass('bot-chat-show');
+    }
+};
+
+jQuery(function () {
+    $('.bot-tab').click(function () {
+        dnnBot.hideTab();
+        setTimeout('dnnBot.showChat()', 200);
+    });
+
+    $('.bot-close').click(function () {
+        dnnBot.hideChat();
+        setTimeout('dnnBot.showTab()', 300);
+    });
+
+});
